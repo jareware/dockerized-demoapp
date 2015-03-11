d8884ce345ec13b493392f28f12030c66b7762ee
Jarno Rantanen <jarno.rantanen@futurice.com>
2015-03-11 18:09:46 +0200

    Add livereload across server & client.

--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,19 @@ var bodyParser = require('body-parser');
 var guestbookApi = require('./api/guestbook');
 var app = express();
 
+if (process.env.ENABLE_LIVERELOAD) {
+    var fs = require('fs');
+    var livereload = require('livereload');
+    var index = fs.readFileSync(__dirname + '/../static/index.html') + '';
+    var inject = fs.readFileSync(__dirname + '/../contrib/livereload-inject.html') + '';
+    livereload
+        .createServer({ interval: 100 }) // @see https://www.npmjs.com/package/livereload#api-options
+        .watch(__dirname + '/../static'); // notify clients about changes to static files
+    app.get('/', function(req, res) { // rewrite any requests to the app root to include the livereload snippet
+        res.send(index.replace('</body>', inject + '</body>')); // append the snippet to the end of <body>
+    });
+}
+
 // @see http://expressjs.com/guide/routing.html
 app.route('/api/guestbook')
     .get(function(req, res) {
